ade557f19ca100b87d85ea508f6311ff9a4a7ef6
Gustavo Villegas <villegasgustavoopel@hotmail.com>
2023-07-07 12:35:06 -0300

    creando validacion del formulario

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+interface UserModel{
+    email:FormControl<string|null>;
+    password: FormControl<string|null>;
+    password2:FormControl<string|null>;
+    address:FormControl<string|null>;
+    address2:FormControl<string|null>;
+    city:FormControl<string|null>;
+    selected:FormControl<string|null>;
+    zip:FormControl<string|null>;
+}
+
+
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +21,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
+ userModel: FormGroup<UserModel>;
 
+ constructor(private fb: FormBuilder){
+  this.userModel = this.fb.group({
+    email:['',Validators.required],
+    password:['',Validators.required],
+    password2:['',Validators.required],
+    address:['',Validators.required],
+    address2:['',Validators.required],
+    city:['',Validators.required],
+    selected:['',Validators.required],
+    zip:['',Validators.required]
+
+
+  })
+ }
 }
