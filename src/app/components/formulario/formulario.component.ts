209a595f76b3f9433856b9b41a40a0f04a5fe63a
Gustavo Villegas <villegasgustavoopel@hotmail.com>
2023-07-11 10:25:12 -0300

    creando errores personalizados

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
-
-interface UserModel{
-    email:FormControl<string|null>;
-    password: FormControl<string|null>;
-    password2:FormControl<string|null>;
-    address:FormControl<string|null>;
-    address2:FormControl<string|null>;
-    city:FormControl<string|null>;
-    selected:FormControl<string|null>;
-    zip:FormControl<string|null>;
+interface UserModel {
+  name: FormControl<string | null>;
+  lastName: FormControl<string | null>;
+  email: FormControl<string | null>;
+  password: FormControl<string | any | null>;
+  password2: FormControl<string | any | null>;
+  address: FormControl<string | null>;
+  address2: FormControl<string | null>;
+  city: FormControl<string | null>;
+  selected: FormControl<string | null>;
+  zip: FormControl<string | null>;
 }
-
-
 
 @Component({
   selector: 'app-formulario',
@@ -21,20 +20,34 @@ interface UserModel{
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
- userModel: FormGroup<UserModel>;
+  userModel: FormGroup<UserModel>;
 
- constructor(private fb: FormBuilder){
-  this.userModel = this.fb.group({
-    email:['',Validators.required],
-    password:['',Validators.required],
-    password2:['',Validators.required],
-    address:['',Validators.required],
-    address2:['',Validators.required],
-    city:['',Validators.required],
-    selected:['',Validators.required],
-    zip:['',Validators.required]
+  constructor(private fb: FormBuilder) {
+    this.userModel = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\d{1,3})(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
+      password2: ['', [Validators.required]],
+      address: ['', Validators.required],
+      address2: ['', Validators.required],
+      city: ['', Validators.required],
+      selected: ['', Validators.required],
+      zip: ['', Validators.required]
+    }, { validators: this.passwordMatchValidator });
+  }
 
-
-  })
- }
+  passwordMatchValidator(group: FormGroup): {[key: string]: any} | null {
+    const password = group.get('password').value;
+    const password2 = group.get('password2').value;
+  
+    if (password !== password2) {
+      group.get('password2').setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    } else {
+      group.get('password2').setErrors(null);
+      return null;
+    }
+  }
 }
+
